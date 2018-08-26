ee02968e846f9c84b6c0b5b8e5b11097d362a3d3
EDGAR <edgar@gmail.com>
2018-08-26 16:09:16 -0500

    Tarea

---
+++ page.php
@@ -0,0 +1,51 @@
+<html>
+ <head>
+  <title>Tareas</title>
+ </head>
+ <body body bgcolor="#d35400">
+ <?php echo '<center><h1>TAREA 1: TIPOS DE VARIABLES EN PHP</h1></center>'; ?>
+ <?php echo '<p>Los nombres de las variables van precedidas del signo $. Los nombres de las variables son sensibles al uso de las mayusculas y minusculas. De tal forma que la sintaxis correcta es:</p>'; ?>
+ <?php echo '<center><p> $nombreVariable = valor;</p></center>'; ?>
+ <?php echo '<p>Las cadenas (string) de caracteres se especifican entre uno de dos tipos de delimitadores (o). Encerrada entre doble comillas  es posible emplear los siguientes caracteres especiales.</p>'; ?>
+ <?php echo '<p>Tipos:</p>'; ?>
+ <?php echo '<p>Entero: Numeros sin decimales. Ejemplo. $anio=1997;</p>'; ?>
+ <?php echo '<p>Real: Numeros en punto flotante. Ejemplo. $gravedad=9.6;</p>'; ?>
+ <?php echo '<p>Alfanumericas: Variable alfanumerica. Ejemplo. $var="e";</p>'; ?>
+ 
+ <?php echo '<center><h1>TAREA 2: OPERADORES EN PHP</h1></center>'; ?>
+ <?php echo '<p>Un operador es algo que toma uno o mas valores y produce otro valor. </p>'; ?>
+ <?php echo '<p>Operadores aritmeticos</p>'; ?>
+ <?php echo '<p>+Suma</p>'; ?>
+ <?php echo '<p>-Resta</p>'; ?>
+ <?php echo '<p>*Multiplicacion</p>'; ?>
+ <?php echo '<p>/Division</p>'; ?>
+ <?php echo '<p>%Modulo(resto de la division)</p>'; ?>
+ <?php echo '<p>Operadores de comparacion</p>'; ?>
+ <?php echo '<p>==Comprueba si son iguales</p>'; ?>
+ <?php echo '<p>!=Comprueba si son distintos</p>'; ?>
+ <?php echo '<p>===Comprueba si son iguales y de exactamente el mismo tipo</p>'; ?>
+ <?php echo '<p>!==Comprueba si son distintos o de distinto tipo</p>'; ?>
+ <?php echo '<p><>Diferente(igual que !=)</p>'; ?>
+ <?php echo '<p><Menor que, comprueba si un valor es menor que el otro</p>'; ?>
+ <?php echo '<p>>Mayor que</p>'; ?>
+ <?php echo '<p><=Menor o igual</p>'; ?>
+ <?php echo '<p>>=Mayor o igual</p>'; ?>
+ <?php echo '<p>Operadores logicos</p>'; ?>
+ <?php echo '<p>and Operacion logica "y", sera verdadero si ambos son verdaderos</p>'; ?>
+ <?php echo '<p>or Operacion logica "o", sera verdadero si uno de ellos es verdadero</p>'; ?>
+ <?php echo '<p>xor Operacion logica "xor", sera verdadero si uno de ellos es verdadero pero no ambos</p>'; ?>
+ <?php echo '<p>!Operacion de negacion, si era verdadero pasa a falso y viceversa</p>'; ?>
+ <?php echo '<p>&&Operacion logica "y"</p>'; ?>
+ <?php echo '<p>||Operacion logica "o"</p>'; ?>
+ 
+ <?php echo '<center><h1>TAREA 3: ARREGLOS EN PHP</h1></center>'; ?>
+ <?php echo '<p>Un array en PHP es en realidad un mapa ordenado. Un mapa es un tipo de datos que asocia valores con claves.</p>'; ?>
+ <?php echo '<p>Un array puede ser creado con el constructor del lenguaje array(). Este toma cualquier numero de parejas clave => valor como argumentos.</p>'; ?>
+ <?php echo '<p>Ejemplo</p>'; ?>
+ <?php echo '<p>array(</p>'; ?>
+ <?php echo '<p>clave  => valor,</p>'; ?>
+ <?php echo '<p>clave2 => valor2,</p>'; ?>
+ <?php echo '<p>clave3 => valor3,</p>'; ?>
+ <?php echo '<p>)</p>'; ?>
+ </body>
+</html>
